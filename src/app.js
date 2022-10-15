10d3bf349cca0f997ffb3ae8e12ef920402fb61a
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-10-15 12:45:38 -0300

    feat: CRUD

--- src/app.js
+++ src/app.js
@@ -4,17 +4,50 @@ const app = express();
 
 app.use(express.json());
 
-const livros = [
-  { id: 1, titulo: "Senhor dos Aneis" },
-  { id: 2, titulo: "O Hobbit" },
+const books = [
+  { id: 1, title: "Lord of Rings" },
+  { id: 2, title: "The Hobbit" },
 ];
 
 app.get("/", (req, res) => {
-  res.status(200).send("Curso de Node");
+  res.status(200).send("Course of Node");
 });
 
-app.get("/livros", (req, res) => {
-  res.status(200).json(livros);
+app.get("/books", (req, res) => {
+  res.json(books);
 });
+
+app.get("/books/:id", (req, res) => {
+  const index = findBook(req.params.id);
+
+  res.json(books[index]);
+});
+
+app.post("/books", (req, res) => {
+  books.push(req.body);
+
+  res.status(201).send("Book created with success!");
+});
+
+app.put("/books/:id", (req, res) => {
+  const index = findBook(req.params.id);
+
+  books[index].title = req.body.title;
+
+  res.json(books);
+});
+
+app.delete("/books/:id", (req, res) => {
+  const { id } = req.params;
+  const index = findBook(id);
+
+  books.splice(index, 1);
+
+  res.send(`Book ${id} deleted with success!`);
+});
+
+function findBook(id) {
+  return books.findIndex((book) => book.id == id);
+}
 
 export default app;
